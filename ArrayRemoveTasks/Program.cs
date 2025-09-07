6a662e41064347d9da9c1eca6f2d9125ba31fa9c
AwallenceCode <awallencecode@gmail.com>
2025-09-07 23:17:54 +0300

    Initial commit

---
+++ ArrayRemoveTasks/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+class MyClass
+{
+    /// <summary>
+    /// method for generating random array 
+    /// </summary>
+    static void GenerateArray(int[] mainArray)
+    {
+        Random rnd = new Random();
+
+        for (int i = 0; i < mainArray.Length; i++)
+        {
+            mainArray[i] = rnd.Next(-21, 21);
+        }
+    }
+
+    /// <summary>
+    /// method for showing array
+    /// </summary>
+    static void ShowArray(int[] mainArray)
+    {
+        Console.WriteLine();
+        
+        for (int i = 0; i < mainArray.Length; i++)
+        {
+            Console.Write(mainArray[i] + " ");
+        }
+    }
+
+    /// <summary>
+    /// method for delete number from the end of an array
+    /// </summary>
+    static void RemoveFromEnd(ref int[] mainArray)
+    {
+        
+        ShowArray(mainArray);
+    }
+
+    /// <summary>
+    /// method for delete number from the start of an array
+    /// </summary>
+    static void RemoveFromStart(ref int[] mainArray)
+    {
+        
+        ShowArray(mainArray);
+    }
+
+    /// <summary>
+    /// method for delete number fron the user index of an array
+    /// </summary>
+    static void RemoveAtIndex(ref int[] mainArray)
+    {
+        
+        ShowArray(mainArray);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter the size your array (0-1000) -> ");
+        if (!int.TryParse(Console.ReadLine(), out int mainSize) || mainSize <= 0 || mainSize > 1000)
+        {
+            Console.WriteLine("Size must be 0 - 1000!");
+            return;
+        }
+
+        int[] mainArray = new int[mainSize];
+
+        GenerateArray(mainArray);
+
+        while (true)
+        {
+            Console.WriteLine("\n\n1 - for showing full array" +
+                              "\n2 - for delete number from the end at an array" +
+                              "\n3 - for delete number from the start at an array" +
+                              "\n4 - for delete number to index of an array" +
+                              "\nWrite \"Exit\" for exit the program");
+
+            Console.Write("\nEnter your choice -> ");
+
+            string userChoice = Console.ReadLine()?.ToLower();
+
+            switch (userChoice)
+            {
+                case "1":
+                    ShowArray(mainArray); break;
+
+                case "2":
+                    RemoveFromEnd(ref mainArray); break;
+
+                case "3":
+                    RemoveFromStart(ref mainArray); break;
+                
+                case "4":
+                    RemoveAtIndex(ref mainArray); break;
+
+                case "exit":
+                    return;
+
+                default:
+                    Console.WriteLine("Try again..."); break;
+            }
+        }
+    }
+}
